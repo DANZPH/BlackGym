4fc4c01f59c729716165dc13fd70c04be9c7c863
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-13 20:03:32 +0800

    remove equipment sidebar.php

--- staff/includes/sidebar.php
+++ staff/includes/sidebar.php
@@ -11,14 +11,14 @@
       </ul>
     </li>
 
-    <li class="<?php if($page=='equipment'){ echo 'submenu active'; } else { echo 'submenu';}?>"> <a href="#"><i class="icon icon-cogs"></i> <span>Gym Equipment</span> </a>
+   <!-- <li class="<?php if($page=='equipment'){ echo 'submenu active'; } else { echo 'submenu';}?>"> <a href="#"><i class="icon icon-cogs"></i> <span>Gym Equipment</span> </a>
       <ul>
         <li><a href="equipment.php">List Gym Equipment</a></li>
         <li><a href="equipment-entry.php">Add Equipment</a></li>
         <li><a href="remove-equipment.php">Remove Equipment</a></li>
         <li><a href="edit-equipment.php">Update Equipment Details</a></li>
       </ul>
-    </li>
+    </li>-->
     <li class="<?php if($page=='membersts'){ echo 'active'; }?>"><a href="member-status.php"><i class="icon icon-eye-open"></i> <span>Member's Status</span></a></li>
     <li class="<?php if($page=='payment'){ echo 'active'; }?>"><a href="payment.php"><i class="icon icon-money"></i> <span>Payments</span></a></li>
     <li class="<?php if($page=='attendance'){ echo 'active'; }?>"><a href="attendance.php"><i class="icon icon-calendar"></i> <span>Manage Attendance</span></a></li>
